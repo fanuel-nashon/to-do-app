8a19ba900d332b1169b114c05359bd92bee577af
fanuel-nashon <fanuelnashon517@gmail.com>
2026-06-30 10:50:00 +0300

    chore: setup db configurations and index files

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config(); // loading environment variables from the .env file
+
+const express = require('express'); // importing express - used to handle http requests
+
+const cors = require('cors'); // import cors - allows react app to talk to node
+
+const app = express(); // creating an express app
+
+const port = process.env.PORT || 3001; // setting up running port
+
+app.use(express.json()); // parsing json requests
+
+app.use(cors ({
+    origin: 'http://localhost:3000'
+})); //
+
+// registering routes
+app.use('');
+
+// health check
+app.get('/health', (req, res)=> {
+    res.json({
+        status: 'ok',
+        timestamp: new Date()
+    });
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server monitor API running on http://localhost:${PORT}`);
+});
