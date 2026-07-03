3f93050b876e42e7be8b0f9e725a1dd024a1044e
fanuel-nashon <fanuelnashon517@gmail.com>
2026-07-03 14:26:49 +0300

    fix: change username to name to match the controller

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
-    username    VARCHAR(50) NOT NULL,
+    name    VARCHAR(50) NOT NULL,
     email       VARCHAR(50) NOT NULL,
-    password    VARCHAR(50) NOT NULL
+    password    VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
