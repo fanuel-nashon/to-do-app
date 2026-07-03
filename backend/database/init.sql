b31a41ba6e9770ea93f0a642823cdfba4ade4454
fanuel-nashon <fanuelnashon517@gmail.com>
2026-07-03 13:12:49 +0300

    fix: removed comma at password column since it is the last

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     id          SERIAL PRIMARY KEY,
     username    VARCHAR(50) NOT NULL,
     email       VARCHAR(50) NOT NULL,
-    password    VARCHAR(50) NOT NULL,
+    password    VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
