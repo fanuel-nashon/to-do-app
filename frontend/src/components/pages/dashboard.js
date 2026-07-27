67f56733a12524923dceea7ac574cba51b963d8a
fanuel-nashon <fanuelnashon517@gmail.com>
2026-07-27 15:46:41 +0300

    feat: add logout functionality

--- frontend/src/components/pages/dashboard.js
+++ frontend/src/components/pages/dashboard.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard(){
-    const token = localStorage.getItem('token');
-    const payload = JSON.parse(atob(token.split('.')[1]));
+    const navigate  = useNavigate();  
+    const [error, setError] = useState(false) 
+
+    const token     = localStorage.getItem('token');
+    let payload     = {};
+
+    if(token) {
+        try {
+            payload   = JSON.parse(atob(token.split('.')[1]));
+        } catch (err) {
+            console.error("Invalid token", err);
+            navigate("/login");
+        }
+    }
+    else {
+        navigate("/login");
+    }
+
+    const handleLogout  = async (e) => {
+        e.preventDefault();
+        try{
+            localStorage.removeItem('token');
+            navigate('/login');
+        } catch (err) {
+            setError("Logout failed");
+        } 
+    };
+
     return(
         <>
             <p>Welcome {payload.name}</p>
+            <form   id="logoutForm"
+                    onSubmit={handleLogout}
+            >
+                <button type="submit">
+                    Logout
+                </button>
+                {error && <p style={{color: 'red' }}> {error}</p>}
+            </form>
         </>
     );
 }
